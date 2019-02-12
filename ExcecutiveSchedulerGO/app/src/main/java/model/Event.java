feb101e817030315c571e8957b3dbe275393d7b0
Atli Marcher Pálsson <atli@james.N17>
2019-02-12 17:45:56 +0000

    bæta við user og event modelklasa

---
+++ ExcecutiveSchedulerGO/app/src/main/java/model/Event.java
@@ -0,0 +1,75 @@
+package model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Event {
+
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private String title;
+    private String description;
+    private List<User> users;
+
+    public Event() {
+
+    }
+
+    public Event(Long id, Date startDate, Date endDate, String title, String description, List<User> users) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.description = description;
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ ExcecutiveSchedulerGO/app/src/main/java/model/User.java
@@ -0,0 +1,72 @@
+package model;
+
+import java.util.List;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String token;
+    private List<Event> events;
+
+    public User() {
+
+    }
+
+    public User(String username, String name, String password, String token) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+}
