09a5b78e5bbb71c6ab44a8cb616d56f4f54abcae
Atli Marcher Pálsson <atli@james.N17>
2019-02-12 18:16:17 +0000

    bætti við takka til að skipta á milli

--- ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/LoginActivity.java
+++ ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.excecutiveschedulergo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mLoginButton = findViewById(R.id.loginButton);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(back);
+            }
+        });
     }
 }
--- ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/MainActivity.java
+++ ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.excecutiveschedulergo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton = findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+
+                startActivity(login);
+            }
+        });
     }
 }
