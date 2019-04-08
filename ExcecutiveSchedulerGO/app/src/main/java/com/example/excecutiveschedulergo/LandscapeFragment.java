afa882fd97e145bb151c058378ee8ae9452504cb
atlimp <amp17@hi.is>
2019-04-08 15:39:50 +0000

    Landscape fragment checkerboard ready

--- ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/LandscapeFragment.java
+++ ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/LandscapeFragment.java
@@ -1,33 +1,20 @@
 package com.example.excecutiveschedulergo;
 
-import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
-import android.content.ReceiverCallNotAllowedException;
-import android.content.res.Resources;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.util.TypedValue;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.Connection.Connection;
 import com.example.model.Event;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
-import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
@@ -45,14 +32,10 @@ import okhttp3.Response;
 
 public class LandscapeFragment extends Fragment {
 
-    //View clickSource;
-    //View touchSource;
-    //ArrayList<ListView> all;
-    //int offset = 0;
-
     FragmentActivity activity;
 
     RelativeLayout[] days;
+    ScrollView mLandscapeScrollview;
 
     Connection c = Connection.getInstance();
 
@@ -103,6 +86,8 @@ public class LandscapeFragment extends Fragment {
 
         initDays(view);
 
+        mLandscapeScrollview = view.findViewById(R.id.landscape_scrollview);
+
         Calendar today = new GregorianCalendar();
 
         startCal = new GregorianCalendar(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
@@ -128,6 +113,11 @@ public class LandscapeFragment extends Fragment {
     }
 
     private void updateUI() {
+        int hour = new GregorianCalendar().get(Calendar.HOUR_OF_DAY);
+
+        int yOff = (int) ((hour - 2) * 30 * getResources().getDisplayMetrics().density);
+        mLandscapeScrollview.smoothScrollTo(0, yOff);
+            
         for (Event e : events) {
             LayoutInflater inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = inflater.inflate(R.layout.event_layout, null);
@@ -149,11 +139,11 @@ public class LandscapeFragment extends Fragment {
             // Number of half hour timeslots
             int startTime = (start.get(Calendar.HOUR_OF_DAY) * 60) + start.get(Calendar.MINUTE);
             // Offset from top in dp
-            int topOff = (int) ((startTime / 30) * 15 * density);
+            int topOff = (int) ((1.0 * startTime / 60) * 30 * density);
 
             long duration = (end.getTimeInMillis() - start.getTimeInMillis()) / (1000 * 60);
             // Height in dp
-            int height = (int) ((duration / 30) * 15 * density);
+            int height = (int) ((1.0 * duration / 60) * 30 * density);
 
             // Week starts at monday
             int index = (start.get(Calendar.DAY_OF_WEEK) + 5) % 7;
@@ -211,108 +201,4 @@ public class LandscapeFragment extends Fragment {
         });
 
     }
-/*
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState){
-        super.onViewCreated(view, savedInstanceState);
-        setFields(view);
-    }
-
-    public void setFields(View view){
-        String[] days = {"labels","monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
-        all = getComponents(view);
-
-        DisplayMetrics dm   = view.getResources().getDisplayMetrics();
-        int width           = dm.widthPixels;
-        int height          = dm.heightPixels;
-
-        int max             = (width > height) ? width : height;
-        int counter         = 0;
-
-        for( ListView lv : all){
-            if(lv.getId() != R.id.labels) {
-                ArrayList<String> list = fillList();
-                ArrayAdapter<String> adapter = createAdapter(list);
-                lv.setAdapter(adapter);
-            } else {
-                ArrayList<String> list = fillLabels();
-                ArrayAdapter<String> adapter = createAdapter(list);
-                lv.setAdapter(adapter);
-            }
-            ViewGroup.LayoutParams lp = lv.getLayoutParams();
-            lp.width = (int) (max / 8);
-            View header = getLayoutInflater().inflate(R.layout.listview_header,null);
-            lv.addHeaderView(header);
-            TextView headerText = header.findViewById(R.id.header);
-            headerText.setText(days[counter++]);
-        }
-        tandem();
-    }
-
-    private ArrayList<ListView> getComponents(View view) {
-        LinearLayout layout = view.findViewById(R.id.list_holder);
-        ArrayList<ListView> result = new ArrayList<>();
-        for (int i = 0; i < layout.getChildCount(); i++) {
-            View v = layout.getChildAt(i);
-            if (v instanceof ListView) {
-                result.add((ListView) v);
-            }
-        } return result;
-    }
-
-    private ArrayList<String> fillList(){
-        ArrayList<String> result = new ArrayList<>();
-        for(int i = 0; i < 24; i++){
-            result.add("");
-        } return result;
-    }
-
-    private ArrayList<String> fillLabels(){
-        ArrayList<String> result = new ArrayList<>();
-        for(int i = 0; i < 24; i++){
-            if( i < 10) result.add( "0" + i + ":00" );
-            else        result.add( i + ":00" );
-        } return result;
-    }
-
-
-    private <T> ArrayAdapter<T> createAdapter(ArrayList<T> day){
-        ArrayAdapter<T> result = new ArrayAdapter<T>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                day
-        );
-        return result;
-    }
-
-    /**
-     * From https://stackoverflow.com/a/12342853
-     * Makes all the lists move together.
-     */
-
-/*
-    private void tandem(){
-        for(ListView lv : all) {
-            lv.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (touchSource == null)
-                        touchSource = v;
-
-                    if (v == touchSource) {
-                        for (ListView other : all) {
-                            if(other != v) other.dispatchTouchEvent(event);
-                        }
-                        if (event.getAction() == MotionEvent.ACTION_UP) {
-                            clickSource = v;
-                            touchSource = null;
-                        }
-                    } return false;
-                }
-            });
-        }
-    }
-
-    */
-
 }
