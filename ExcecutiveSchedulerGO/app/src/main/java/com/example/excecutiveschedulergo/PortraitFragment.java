9aa79a87a8576f08f0b094c0ce66e2659aefbe0e
G10ckm4n <Pollinn@Gmail.com>
2019-04-14 15:22:45 +0000

    Update PortraitFragment.java

--- ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/PortraitFragment.java
+++ ExcecutiveSchedulerGO/app/src/main/java/com/example/excecutiveschedulergo/PortraitFragment.java
@@ -1,32 +1,25 @@
 package com.example.excecutiveschedulergo;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import com.example.Connection.Connection;
 import com.example.adapters.ListViewAdapter;
 import com.example.model.Event;
 import com.example.model.User;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.text.DateFormat;
@@ -36,36 +29,38 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
-
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
-
+/**
+ * Shows events that take place on the specified day
+ */
 public class PortraitFragment extends Fragment {
 
-    View mPage;
+    FragmentActivity activity;          // Parent activity
+    View view;                          // Fragment xml
+    ListView mList;                     // Shows events
+    LinearLayout mCardView;             // Shows longClicked event details
+    Button mPrevButton;                 // Show previous day
+    Button mNextButton;                 // Show next day
+    TextView mCurrentDate;              // Shows chosen day
+    ProgressBar mCalendarProgress;      // Shown while loading events from backend
 
-    ListView mList;
+    Calendar startCal;                  // Delimits event dates to search for
+    Calendar endCal;                    // when sending request to backend
+
+    // Singleton connection to backend
     Connection c = Connection.getInstance();
-
-    FragmentActivity activity;
-    View view;
-
-    Button mPrevButton;
-    Button mNextButton;
-    TextView mCurrentDate;
-    ProgressBar mCalendarProgress;
-
-    Calendar startCal;
-    Calendar endCal;
-
-    LinearLayout mCardView;
 
     public PortraitFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Sets parent activity
+     * @param activity
+     */
     public void setActivity(FragmentActivity activity) {
         this.activity = activity;
     }
@@ -74,9 +69,10 @@ public class PortraitFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        // Set xml fragment
         view = inflater.inflate(R.layout.fragment_portrait, container, false);
 
-        mPage = activity.findViewById(R.id.page_root);
+        // Set components
         mList = view.findViewById(R.id.list);
 
         mPrevButton = view.findViewById(R.id.prevDayButton);
@@ -85,22 +81,29 @@ public class PortraitFragment extends Fragment {
         mCurrentDate = view.findViewById(R.id.currentDate);
 
         mCalendarProgress = view.findViewById(R.id.calendarProgress);
+        mCardView = view.findViewById(R.id.CardView);
 
+        // Set current day
         Calendar today = new GregorianCalendar();
 
         startCal = new GregorianCalendar(today.get(Calendar.YEAR), today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH));
         endCal = (Calendar) startCal.clone();
         endCal.add(Calendar.DATE, 1);
 
-        mCardView = view.findViewById(R.id.CardView);
-
+        // Set component listeners
         setListeners();
 
+        // Reload list with events from current day
         reloadData();
 
         return view;
     }
 
+    /**
+     * Makes a string out of a date
+     * @param date
+     * @return
+     */
     private String dateString(Date date) {
         Locale loc = new Locale("is", "IS");
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.DEFAULT, loc);
@@ -109,26 +112,22 @@ public class PortraitFragment extends Fragment {
         return dateFormat.format(date) + " " + timeFormat.format(date);
     }
 
+    /**
+     * Sets all component listeners
+     */
     private void setListeners(){
-        mPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
-
-
-
+        // When event is clicked, the toolbars current event is set to the clicked item
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 activity.toolbar.event = (Event) parent.getItemAtPosition(position);
-                mCardView.setVisibility(View.GONE);
+                mCardView.setVisibility(View.GONE);         // Hide event details idempotent
             }
         });
 
-
+        // Populate cardView component with clicked items details
         mList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id){
@@ -148,6 +147,7 @@ public class PortraitFragment extends Fragment {
                 ListView mUserList = whole.findViewById(R.id.CardUsers);
                 List<User> users = event.getUsers();
 
+                // Adapter for userlist inside cardView
                 ArrayAdapter<User> adapter = new ArrayAdapter<User>(
                         activity.getApplicationContext(),
                         android.R.layout.simple_list_item_1,
@@ -159,6 +159,7 @@ public class PortraitFragment extends Fragment {
             }
         });
 
+        // Hide cardView when clicked
         mCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -166,6 +167,7 @@ public class PortraitFragment extends Fragment {
             }
         });
 
+        // Show events of previous day
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -175,6 +177,7 @@ public class PortraitFragment extends Fragment {
             }
         });
 
+        // Show events of next day
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -185,6 +188,9 @@ public class PortraitFragment extends Fragment {
         });
     }
 
+    /**
+     * Gets relevant events from backend and populates mList with them
+     */
     private void reloadData() {
         // Set progress bar visibility
         mCalendarProgress.setVisibility(View.VISIBLE);
@@ -196,6 +202,7 @@ public class PortraitFragment extends Fragment {
 
         mCurrentDate.setText("" + startCal.get(Calendar.DAY_OF_MONTH) + "." + startCal.get(Calendar.MONTH) + "." + startCal.get(Calendar.YEAR));
 
+        // Authentication for backend
         String token = TokenStore.getToken(activity.getApplicationContext());
         c.getEvents(new Date(startCal.getTimeInMillis()), new Date(endCal.getTimeInMillis()), token, new Callback() {
             @Override
